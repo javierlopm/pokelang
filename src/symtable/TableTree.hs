c34e960b5fec10ab99a2f5556c3e81c802b14b68
Javier Lopez <javierlopm@hotmail.com>
2016-05-10 20:25:58 -0400

    Cambios en el arbol

--- src/symtable/TableTree.hs
+++ src/symtable/TableTree.hs
@@ -1,6 +1,14 @@
 module TableTree(
     Scope  (..),
-    Zipper (..)
+    Zipper (..),
+    newtable,
+    addEntry,
+    emptyScope,
+    enterScope,
+    insert,
+    fromScope,
+    down,
+    apply
     ) where
 
 import qualified Data.Map.Strict as Map
@@ -16,8 +24,8 @@ data Scope a = Scope (SymbolTable a) [Scope a] -- Y si... usamos sequence aqui p
              deriving (Show) -- Sustituir por el show mostrado en clases
 
 data Breadcrumb a = Breadcrumb { left  :: [Scope a]
-					    	   , right :: [Scope a]
-					    	   , action:: Action
+					    	   , right :: [Scope a] -- El primero en right ser'a el ultimo visitado
+					    	   --, action:: Action
 						       }
 				  deriving(Show)	
 
@@ -25,7 +33,7 @@ data Breadcrumb a = Breadcrumb { left  :: [Scope a]
 --                   deriving(Show)
 
 -- Zipper como nodo actual y tupla (Tabla de padre, hermanos a la izq y a la der
-type Zipper a = (Scope a, Breadcrumb a)
+type Zipper a = (Scope a, [Breadcrumb a])
 
 -- Symbol table
 newtable :: SymbolTable a
@@ -39,28 +47,29 @@ emptyScope :: Scope a
 emptyScope = Scope newtable []
 
 enterScope :: Scope a -> Scope a
-enterScope (Scope symtable []) = Scope symtable (emptyScope:[])
-enterScope (Scope symtable (lst@(Scope stc chld):childL)) = Scope symtable (emptyScope:lst:childL)
+enterScope (Scope symtable l)  = Scope symtable (emptyScope:l) -- Isn't it shorter?
+-- enterScope (Scope symtable []) = Scope symtable (emptyScope:[])
+-- enterScope (Scope symtable (lst@(Scope stc chld):childL)) = Scope symtable (emptyScope:lst:childL)
 
 insert :: String -> a -> Scope a -> Scope a
 insert key val (Scope symtable chl) = Scope (addEntry key val symtable) chl
 
 -- Zipper
-fromSymTable :: Scope a -> Zipper a
-fromSymTable = flip (,) $ Breadcrumb [] [] None
+fromScope :: Scope a -> Zipper a
+fromScope = flip (,) $ [] 
 
 -- No funciona -- Revisar si conviene trabajar con ST o con Scopes
 down :: Zipper a -> Maybe (Zipper a)
-down (Scope symt [] , breadcrumbs) = Nothing
-down (Scope symt (ch:chdrn) , breadcrumbs) = Just (ch,(oldstep:breadcrumb))
-    where oldstep = (Scope symt (ch:chdrn) chdrn Down) --COnvertir a nuevo breadcrum y manejarlo asi.
+down (Scope symt [] , breadcrumbs)         = Nothing
+down (Scope symt (ch:chdrn) , breadcrumbs) = Just (ch,((Breadcrumb [] [oldstep]):breadcrumbs))
+    where oldstep = (Scope symt chdrn) --Al subir volvemos a armar el hijo 
 
 apply :: (Scope a -> Scope a) -> Zipper a -> Zipper a
 apply f (scope,breadcrumbs) = (f scope,breadcrumbs)
 
 -- Corrida so far
 -- Crear un nodo vacio, convertirlo en zipper, insertar hola con 42, crearle un hijo, ir al hijo
--- apply enterScope  $ apply (insert "hola" 42) $ fromSymTable  emptyScope
+-- apply enterScope  $ apply (insert "hola" 42) $ fromScope  emptyScope
 
 test = newtable
 test2 = addEntry "elem1" 1 test
@@ -69,4 +78,4 @@ s1 = emptyScope
 s2 = insert "elem1" 1 s1
 s3 = insert "elem2" 2 s2
 s4 = enterScope s3
-z1 = fromSymTable s3
+z1 = fromScope s3
