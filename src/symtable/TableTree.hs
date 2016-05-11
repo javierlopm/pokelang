7b500359ce46f4e45c46e2c7b901b783e3633c38
Nabil J. Marquez <nabiljesus@gmail.com>
2016-05-10 21:49:38 -0430

    Down 'ready?'

--- src/symtable/TableTree.hs
+++ src/symtable/TableTree.hs
@@ -9,7 +9,7 @@ import Data.Maybe
 
 type SymbolTable a = Map.Map String a
 
-data Action = UpA | DownA | LeftA | RightA | RootA
+data Action = UpA | DownA | LeftA | RightA | RootA | StChild
 				  deriving(Show)	
 
 data Scope a = Scope (SymbolTable a) [Scope a] -- Y si... usamos sequence aqui para tenerlos ordenados?
@@ -47,14 +47,16 @@ insert key val (Scope symtable chl) = Scope (addEntry key val symtable) chl
 
 -- Zipper
 fromSymTable :: Scope a -> Zipper a
-fromSymTable orig@(Scope symt chdrn)= (orig,Breadcrumb [] chdrn [RootA]) 
+fromSymTable orig@(Scope symt chdrn)= (orig,Breadcrumb [] chdrn [RootA] )
+--fromSymTable orig@(Scope symt chdrn)= (orig,Breadcrumb [] chdrn ((map (\x -> StChild) chdrn)++[RootA])) 
 
 -- No funciona -- Revisar si conviene trabajar con ST o con Scopes
 down :: Zipper a -> Maybe (Zipper a)
 down (Scope symt [] , breadcrumbs) = Nothing
 down (Scope symt (ch:chdrn) , breadcrumbs) = Just (ch,newBread)
     where 
-    	  newBread = Breadcrumb (symt:(left breadcrumbs)) (chdrn++(right breadcrumbs)) (DownA:(action breadcrumbs)) 
+          newBread = Breadcrumb (symt:(left breadcrumbs)) (chdrn++(right breadcrumbs)) (DownA:((map (\x -> StChild) chdrn)++(action breadcrumbs)) )
+    	  --newBread = Breadcrumb (symt:(left breadcrumbs)) (chdrn++(right breadcrumbs)) (DownA:(action breadcrumbs)) 
 
 --apply :: (Scope a -> Scope a) -> Zipper a -> Zipper a
 --apply f (scope,breadcrumbs) = (f scope,breadcrumbs)
@@ -66,11 +68,16 @@ down (Scope symt (ch:chdrn) , breadcrumbs) = Just (ch,newBread)
 test = newtable
 test2 = addEntry "elem1" 1 test
 test3 = addEntry "elem2" 2 test2
+test4 = addEntry "elem3" 3 test3
 s1 = emptyScope
 s2 = insert "elem1" 1 s1
 s3 = insert "elem2" 2 s2
 s4 = insert "elem3" 3 s3
 s5 = enterScope s4
 z1 = fromSymTable s5
-z12 = down z1
-rz12 = fromJust z12
+aux1 = newtable
+aux12 = addEntry "I'm Hijo" 1 aux1
+aux2 = addEntry "I'm 2 Hijo" 2 aux1
+z1_2 = ((Scope test4 [(Scope aux1 []),(Scope aux2 [])])     ,    (Breadcrumb {left = [], right = [(Scope aux1 [])], action = [RootA]})     )   --Nodo con dos hijos
+z1d = down z1_2
+z1c = fromJust z1d
