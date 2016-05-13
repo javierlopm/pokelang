b50ea832a696be4902c5dc721ece868e387e1974
Nabil J. Marquez <nabiljesus@gmail.com>
2016-05-13 11:22:21 -0400

    Hay secuences

--- src/symtable/TableTree.hs
+++ src/symtable/TableTree.hs
@@ -17,7 +17,8 @@
 
 import qualified Data.Map.Strict as Map
 import Data.Foldable(toList)
-import Data.Sequence(Seq,empty,(|>),(<|),ViewL((:<)),ViewR((:>)),(><),viewl)
+import qualified Data.Sequence as DS
+import Data.Sequence(Seq,empty,(|>),(<|),ViewL((:<)),ViewR((:>)),(><),viewl,length)
 
 import Data.Maybe(fromJust,isNothing)
 import Data.List (intercalate)
@@ -86,12 +87,12 @@ fromZipper = fst . goTop
 
 -- No funciona -- Revisar si conviene trabajar con ST o con Scopes
 goDown :: Zipper a -> Maybe (Zipper a)
-goDown (Scope symt chls , breadcrumbs) = Just (ch,newBread)
+goDown (Scope symt chls , breadcrumbs) | DS.null chls  = Nothing
+                                       | otherwise  = Just (ch,newBread)
     where (ch :< chdrn) = viewl chls
           newBread = Breadcrumb ( Scope symt empty : left breadcrumbs )
                                 (chdrn >< right breadcrumbs) 
-                                ( replicate (length chdrn) (StChild)  ++ (DownA:(action breadcrumbs)))  --Guarda ST sin hijos para luego ponerselos al subir
-goDown (Scope symt children , breadcrumbs) =  Nothing
+                                ( replicate (Data.Sequence.length chdrn) (StChild)  ++ (DownA:(action breadcrumbs)))  --Guarda ST sin hijos para luego ponerselos al subir
         --newBread = Breadcrumb (symt:(left breadcrumbs)) (chdrn++(right breadcrumbs)) (DownA:(action breadcrumbs)) 
 
 apply :: (Scope a -> Scope a) -> Zipper a -> Zipper a
@@ -102,19 +103,21 @@ goRight (scp, breadcrumbs) = case snd brk2 of
                              StChild:_ -> wentR
                              otherwise -> Nothing
     where acts  = (action breadcrumbs)
-          wentR = Just $ ((head (right breadcrumbs)), newBread)
+          wentR = Just $ (hrBr, newBread)
+          (hrBr :< lrBr) = viewl (right breadcrumbs)
           brk1  = break (\x -> (x== DownA) ||(x==RootA)) acts                      
           brk2  = break (/= RightA) $ fst brk1
           newBread = Breadcrumb 
                       (scp:(left breadcrumbs)) 
-                      (tail (right breadcrumbs)) $ 
+                      lrBr $ 
                             (RightA : fst brk2) ++ (tail (snd brk2)) ++ snd brk1
 
 goLeft :: Zipper a -> Maybe (Zipper a)
 goLeft (scp, (Breadcrumb lft rgt [])) = Nothing
-goLeft (scp, (Breadcrumb lft rgt (RightA:lact))) = Just ((head lft),(Breadcrumb (tail lft) (scp:rgt) nAct))
+goLeft (scp, (Breadcrumb lft rgt (RightA:lact))) = Just ((head lft),(Breadcrumb (tail lft) (scp<|rgt) nAct))
     where acts  = (RightA:lact)
           brk1  = break (\x -> (x== DownA) ||(x==RootA)) acts
+
           brk2  = break (/= RightA) $ fst brk1
           nAct  = (tail (fst brk2) ++ ((snd brk2)) ++ StChild:snd brk1)            --RightA,RightA,StChild,goDown,...
 goLeft (scp, (Breadcrumb lft rgt brc)) = Nothing
@@ -122,12 +125,14 @@ goLeft (scp, (Breadcrumb lft rgt brc)) = Nothing
 getST :: Scope a -> SymbolTable a
 getST (Scope st chld) = st
 
-getChld :: Scope a -> [Scope a]
+getChld :: Scope a -> Seq (Scope a)
 getChld (Scope st chld) = chld
 
-wentUp :: Zipper a -> [Scope a] -> Zipper a
-wentUp (scp, (Breadcrumb lft rgt (DownA:lact)))   acc = ((Scope (getST (head lft)) (scp:reverse acc)),(Breadcrumb (tail lft) rgt lact ))
-wentUp (scp, (Breadcrumb lft rgt (StChild:lact))) acc = wentUp (scp, (Breadcrumb lft (tail rgt) (lact))) ((head rgt):acc)
+wentUp :: Zipper a -> Seq(Scope a) -> Zipper a
+wentUp (scp, (Breadcrumb lft rgt (DownA:lact)))   acc = ((Scope (getST (head lft)) (scp<|acc)),(Breadcrumb (tail lft) rgt lact ))
+wentUp (scp, (Breadcrumb lft rgt (StChild:lact))) acc = wentUp (scp, (Breadcrumb lft lrBr (lact))) (acc|>hrBr)
+              where 
+                (hrBr :< lrBr) = viewl rgt
 
 wentLeft :: Zipper a -> Zipper a
 wentLeft inp@(scp, (Breadcrumb lft rgt (RightA:lact))) = wentLeft $ fromJust $ goLeft inp
@@ -136,9 +141,9 @@ wentLeft inp@(scp, (Breadcrumb lft rgt (oact)))        = inp
 goUp :: Zipper a -> Maybe (Zipper a)
 goUp (scp, (Breadcrumb lft rgt []))      = Nothing
 goUp (scp, (Breadcrumb lft rgt [RootA])) = Nothing
-goUp inp@(scp, (Breadcrumb lft rgt (DownA:lact)))   = Just (wentUp inp [])
-goUp inp@(scp, (Breadcrumb lft rgt (StChild:lact))) = Just (wentUp inp [])
-goUp inp@(scp, (Breadcrumb lft rgt (RightA:lact)))  = Just (wentUp (wentLeft inp) [])
+goUp inp@(scp, (Breadcrumb lft rgt (DownA:lact)))   = Just (wentUp inp empty)
+goUp inp@(scp, (Breadcrumb lft rgt (StChild:lact))) = Just (wentUp inp empty)
+goUp inp@(scp, (Breadcrumb lft rgt (RightA:lact)))  = Just (wentUp (wentLeft inp) empty)
 goUp (scp, (Breadcrumb lft rgt brc))     = Nothing
 
 goTop :: Zipper a ->  Zipper a
