2291390de3e47922cb39322528c61cdcd0027932
Javier <javierloplom@gmail.com>
2016-09-18 12:48:53 -0400

    New mem access structure

--- src/instructions/Tac.hs
+++ src/instructions/Tac.hs
@@ -1,25 +1,29 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Tac(
-    IntIns(..)
+    IntIns(..),
+    Memory
 ) where 
 
-data Memory = MemDir       Int      -- Plain memory direction
-            | MemOffs      Int Int  -- Memory with offset
-            | MemIndir     Int      -- Pointer Follow
-            | MemIndirOffs Int Int  -- Follow pointer, then add offset
-            | MemLabel     String   -- Data label 
-            | MemLabelOffs Int String  -- Add offset to data label
+import Data.Sequence
+import Data.Binary
 
-instance Show Memory where
-    show (MemDir       m    ) = show m
-    show (MemOffs      m o  ) = '(' : (show o) ++ ')' : show m 
-    show (MemIndir     m    ) = '*' : show m
-    show (MemIndirOffs m o  ) = '*' : '(' : show o ++ ')' : show m 
-    show (MemLabel     str  ) = str 
-    show (MemLabelOffs o str) = "(" ++ show o ++ ")" ++ str 
+data Memory = MemIndexR      Int Int    --   0(R0) 0 + contents(R0)
+            | MemIndex       String Int --  lb(R0) lb + contents(R0)
+            | MemIndirIndex  String Int -- *lb(R0) contents(lb + contents(R0))
+            | MemIndirIndexR Int Int    --  *0(R1) contents( 0 + contents(R1))
+
+
+instance Show      Memory where
+    show (MemIndexR      o  r0) = show  o ++ "(R" ++ show r0 ++ ")"
+    show (MemIndex       lb r0) =       lb ++ "(R" ++ show r0 ++ ")"
+    show (MemIndirIndex  lb r0) = '*' :      lb ++ "(R" ++ show r0 ++ ")"
+    show (MemIndirIndexR o  r0) = '*' : show  o ++ "(R" ++ show r0 ++ ")"
+
 
 newtype ConsVal = Cons Int
 
-instance Show ConsVal where
+instance Show      ConsVal where
     show (Cons a) = '#' : show a 
 
             -- Dest Src1 Src2  -  Reg,Reg,Reg
@@ -63,6 +67,7 @@ data IntIns = FlMult   Int Int Int  -- Float
             -- Src Dest
             | Load     Int Memory -- Loading oper
             | Store    Memory Int
+            | Mv       Int    Int
             -- Load long value into registers
             | Loadi    Int ConsVal
             -- Function calls
@@ -72,7 +77,7 @@ data IntIns = FlMult   Int Int Int  -- Float
             | Comment String
             | Nop 
 
-instance Show IntIns where
+instance Show      IntIns where
     show (FlMult   r0 r1 r2)  =  showTAC r0 r1 "f*" r2 
     show (FlAdd    r0 r1 r2)  =  showTAC r0 r1 "f+" r2 
     show (FlSub    r0 r1 r2)  =  showTAC r0 r1 "f-" r2 
@@ -92,32 +97,34 @@ instance Show IntIns where
     show (LEq      r0 r1 r2)  =  showTAC r0 r1 "<=" r2
     show (GEq      r0 r1 r2)  =  showTAC r0 r1 ">=" r2
     show (Jump     str    )   = "goto " ++ str
-    show (Jz       r0 str )   = "if $" ++ show r0 ++ "z  goto" ++ str 
-    show (Jnotz    r0 str )   = "if $" ++ show r0 ++ "nz goto" ++ str 
+    show (Jz       r0 str )   = "if R" ++ show r0 ++ "z  goto" ++ str 
+    show (Jnotz    r0 str )   = "if R" ++ show r0 ++ "nz goto" ++ str 
     show (JLt      r0 r1 str) = showIf r0 r1 "<"  str
     show (JGt      r0 r1 str) = showIf r0 r1 ">"  str
     show (JLEq     r0 r1 str) = showIf r0 r1 "<=" str
     show (JGEq     r0 r1 str) = showIf r0 r1 ">=" str
-    show (Addi     r0 r1 i) = showTAC r0 r1 "+" i
-    show (Subi     r0 r1 i) = showTAC r0 r1 "-" i
-    show (Multi    r0 r1 i) = showTAC r0 r1 "*" i
-    show (Divi     r0 r1 i) = showTAC r0 r1 "/" i
-    show (Addf     r0 r1 f) = showTAC r0 r1 "f+" f
-    show (Subf     r0 r1 f) = showTAC r0 r1 "f-" f
-    show (Multf    r0 r1 f) = showTAC r0 r1 "f*" f
-    show (Divf     r0 r1 f) = showTAC r0 r1 "f/" f
-    show (Load     r0 m) = "LD $" ++ show r0 ++ " " ++ show m
-    show (Store    m r0) = "ST "  ++ show m  ++ " $" ++ show r0
-    show (Loadi    r0 c) = "LDI $" ++ show r0 ++ show c
-    show (Call     str ) = "CALL " ++ str
-    show (Param    par ) = "PARAM$" ++ show par
-    show (Comment  str ) = ';': str
-    show Nop  = "NoOp"
+    show (Addi     r0 r1 i)   = showTAC r0 r1 "+"  i
+    show (Subi     r0 r1 i)   = showTAC r0 r1 "-"  i
+    show (Multi    r0 r1 i)   = showTAC r0 r1 "*"  i
+    show (Divi     r0 r1 i)   = showTAC r0 r1 "/"  i
+    show (Addf     r0 r1 f)   = showTAC r0 r1 "f+" f
+    show (Subf     r0 r1 f)   = showTAC r0 r1 "f-" f
+    show (Multf    r0 r1 f)   = showTAC r0 r1 "f*" f
+    show (Divf     r0 r1 f)   = showTAC r0 r1 "f/" f
+    show (Mv       r0 r1  )   = "MV R" ++ show r0 ++ " R" ++ show r1
+    show (Load     r0 m)      = "LD R" ++ show r0 ++ " " ++ show m
+    show (Store    m r0)      = "ST "  ++ show m  ++ " R" ++ show r0
+    show (Loadi    r0 c)      = "LDI R" ++ show r0 ++ show c
+    show (Call     str )      = "CALL " ++ str
+    show (Param    par )      = "PARAM R" ++ show par
+    show (Comment  str )      = ';': str
+    show Nop                  = "NoOp"
 
-shwAsgn int = "$" ++ show int ++ ":="
+-- Print auxiliaries
+shwAsgn int = "R" ++ show int ++ ":="
+showTAC  r0 r1 s r2 = 'R' : show r0 ++ " := R" ++ show r1 ++ " " ++ s ++ " R" ++ show r2
+showTACi r0 r1 s i  = 'R' : show r0 ++ " := R" ++ show r1 ++ " " ++ s ++ " #" ++ show i
+show2AC  r0 r1 s    = 'R' : show r0 ++ " := "  ++ s ++" R" ++  show r1
+showIf r0 r1 s label = "if R" ++ show r0 ++ " " ++ s ++ " R" ++ show r1 ++ " goto " ++ show label
 
-showTAC  r0 r1 s r2 = '$' : show r0 ++ " := $" ++ show r1 ++ " " ++ s ++ " $" ++ show r2
-showTACi r0 r1 s i  = '$' : show r0 ++ " := $" ++ show r1 ++ " " ++ s ++ " #" ++ show i
-show2AC  r0 r1 s    = '$' : show r0 ++ " := "  ++ s ++" $" ++  show r1
-
-showIf r0 r1 s label = "if $" ++ show r0 ++ " " ++ s ++ " $" ++ show r1 ++ " goto " ++ show label
+type Program = Seq IntIns
