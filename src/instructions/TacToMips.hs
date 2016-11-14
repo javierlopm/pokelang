2303f2c41914c37ee524eeca8fc366a21ad7cf59
Javier <javierloplom@gmail.com>
2016-11-13 22:06:07 -0400

    some templates added. added descriptor update functions and register find

--- src/instructions/TacToMips.hs
+++ src/instructions/TacToMips.hs
@@ -1,43 +1,67 @@
+{-# LANGUAGE OverloadedStrings #-}
 module TacToMips(module TacToMips) where
 
-import Prelude hiding(null,replicate)
+import Prelude hiding(replicate)
+import Control.Monad.State
 import Data.Word
-import Data.Sequence hiding(empty,replicate)
-import qualified Data.Sequence as S(empty)
-import qualified Data.Vector   as V(empty)
+import Tac
+import qualified Data.Text     as T
+import qualified Data.Sequence as S(empty,null)
+import qualified Data.Vector   as V(empty,null)
 import qualified Data.Map      as M(empty)
 import Data.Foldable as F(foldl)
 import Data.Foldable (toList)
 import Data.Functor  (fmap)
-import Data.Vector     hiding(toList,(++),concat,null,empty)
-import Data.Map.Strict hiding(toList,null,empty)
-import Tac
-import Control.Monad.State
+import Data.Sequence   hiding(replicate,null,empty)
+import Data.Vector     hiding(toList,(++),concat,null,empty,any)
+import Data.Map.Strict hiding(toList,null,empty,findIndex,(!))
 
 
-type Mips       = String
-type Register   = Word
-type Registers  = [Word]
+type Mips       = T.Text
+type Register   = Int
+type Registers  = [Int]
 
+lowreg  = 8
+highreg = 23
 
-fp :: Word
+fp :: Register
 fp = 31
-sp :: Word
+sp :: Register
 sp = 30
 
 
 compile :: Program -> Program -> Mips -> Mips
-compile globs program crt = ".data\n"   ++ translate globs      ++ 
-                        "\n.text\n" {-++ translate programcrt ++
+compile globs program crt = T.concat [".data\n"
+                                     ,translate globs
+                                     ,"\n.text\n"
+                                     ,crt]
+                    {-++ translate programcrt ++
                         "\n"        ++ crt-}
-                        ++ crt
+            -- crt
 
 translate :: Program -> Mips
 translate  = undefined
 
--- template :: Program -> [Register] -> Mips
--- template (Addi     Dest Src1 Src2)  = "addi " 
--- template (Subi     Dest Src1 Src2)  = ""
+{- auxiliaries for templates -}
+(~~) :: Mips -> Mips -> Mips
+a ~~ b = a `T.append` b
+
+stt :: Show a => a -> Mips
+stt = T.pack . show
+
+build3Mips :: Mips -> Register -> Register -> Register -> Mips
+build3Mips m r1 r2 r3 = m~~"$"~~(stt r1)~~", $"~~(stt r2)~~", $"~~(stt r3)~~"\n"
+
+buildiMips :: Mips -> Register -> Register -> Int -> Mips
+buildiMips m r1 r2 c = m~~"$"~~(stt r1)~~", $"~~(stt r2)~~", "~~(stt c)~~"\n"
+
+template :: IntIns -> [Register] -> Mips
+template (Addi _ _ (Int_Cons c)) [r1,r2] = buildiMips "addi" r1 r2 c
+template (Subi _ _ (Int_Cons c)) [r1,r2] = buildiMips "addi" r1 r2 (-c)
+template (Addi _ (Int_Cons c) _) [r1,r2] = buildiMips "addi" r1 r2 c
+template (Subi _ (Int_Cons c) _) [r1,r2] = buildiMips "addi" r1 r2 (-c)
+template (Subi _ _ _) [r1,r2,r3]  = build3Mips "sub" r1 r2 r3
+template (Addi _ _ _) [r1,r2,r3]  = build3Mips "add" r1 r2 r3
 -- template (Divi     Dest Src1 Src2)  = ""
 -- template (Mod      Dest Src1 Src2)  = ""
 -- template (Multi    Dest Src1 Src2)  = ""
@@ -51,8 +75,8 @@ partition program = build $ F.foldl includeInLast (S.empty,S.empty) program
             | isJump    ins = (prg |> actualBlock,   S.empty |> ins)
             | otherwise     = (prg, actualBlock |> ins)
           build (blocks,ablock)
-            | null ablock  = blocks |> ablock
-            | otherwise    = blocks 
+            | S.null ablock = blocks |> ablock
+            | otherwise     = blocks 
 -- despues de un jump, nuevo elemento
 -- en un tag         , nuevo elemento
 
@@ -68,18 +92,58 @@ showPartitions prg = concat $ toList $ fmap (\ p -> (showP p)++ "\n----------\n"
 
 {- Inicio de getReg -}
 
-data GetReg = GetReg { regDescriptor :: Vector [Var]
-                     , varDescriptor :: Map Var ([Var],[Register]) } 
+data Descriptor = Descriptor { regDescriptor  :: Vector [Var]
+                             , varDescriptor  :: Map Var ([Var],[Register])
+                             , assembly       :: Mips } 
                      deriving (Show)
 
+
+addOffset :: Int -> Maybe Int
+addOffset a = Just (a+lowreg)
+
+findVar :: Var -> Vector [Var] -> Maybe(Register)
+findVar var v = (findIndex (any (var==)) v) >>= addOffset 
+
+findEmpty :: Vector [Var] -> Maybe(Register)
+findEmpty v = (findIndex null v) >>= addOffset
+
+findRegister :: Var -> Vector [Var] -> Register
+findRegister var v = maybe (maybe (error "no hay spills") id (findEmpty v))
+                           id
+                           (findVar var v)
+
+
 newRegisters :: Vector [Var]
-newRegisters = replicate 32 []
+newRegisters = replicate 16 [] -- 23-8+1
 
-initDescriptor :: GetReg
-initDescriptor = GetReg newRegisters M.empty
+initDescriptor :: Descriptor
+initDescriptor = Descriptor newRegisters M.empty T.empty
 
-type MipsGenerator = StateT GetReg IO
+type MipsGenerator = StateT Descriptor IO
 
-cleanDescriptor :: MipsGenerator ()
-cleanDescriptor = put $ GetReg newRegisters M.empty
+killVar :: Var -> MipsGenerator()
+killVar _ = undefined
 
+copyDescriptor :: Var -> Var -> MipsGenerator()
+copyDescriptor = undefined
+
+updateRegDescriptors :: Int -> ([Var] -> [Var]) -> MipsGenerator()
+updateRegDescriptors reg func = do 
+    registers <- gets regDescriptor
+    state     <- get 
+    put state { regDescriptor = registers // [(reg,(func (registers ! reg)))]}
+
+emit :: Mips -> MipsGenerator()
+emit code = do
+    acc    <- gets assembly
+    state  <- get
+    put state { assembly = T.append acc code }
+
+-- clearDescriptor :: MipsGenerator ()
+-- clearDescriptor = put $ Descriptor newRegisters M.empty 
+
+getReg :: [Var] -> MipsGenerator([Registers])
+getReg (lval:rval:[])      = undefined
+getReg (lval:exp1:exp2:[]) = undefined
+
+-- translate :: Seq(Program) -> MipsGenerator(Mips)
